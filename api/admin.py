6cc54e57740bafa82f7778065388028b51a750d9
khalid ali khan <khalidbinalikhan@gmail.com>
2024-02-15 18:02:59 +0500

    admin product string represantion inhancements

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Product
 
-@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('__str__',)
+  list_display = ("title", "price", "description", "created_at", "updated_at")
+  
+admin.site.register(Product, ProductAdmin)
